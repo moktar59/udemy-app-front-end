7c4b6117e3d1eb6cde59af2393c863eecfac8372
moktar <moktar@bglobal.com>
2020-10-29 00:23:59 +0600

    Upto Slide 20 completed

--- src/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/second">Second</router-link>
-        <router-view></router-view>
-    </div>
-</template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,11 +1,19 @@
 <template>
-  <div id="app">
-    <nav class="navbar bg-white border-bottom navbar-light">
+    <div>
+        <nav class="navbar bg-white border-bottom navbar-light">
             <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">Home</router-link>
             <router-link class="btn nav-button" :to="{name: 'second'}">Second</router-link>
         </nav>
+        <br/>
         <div class="container mt-4 mb-4 pr-4 pl-4">
+            <h1>This is test</h1>
           <router-view></router-view>
         </div>
-  </div>
+    </div>
 </template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
